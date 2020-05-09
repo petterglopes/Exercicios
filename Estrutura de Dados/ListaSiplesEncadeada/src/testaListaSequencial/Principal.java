7e397988e4e438a02ba7785e9e712c9584ada559
Petter <tekhardware@hotmail.com>
2020-05-08 22:27:44 -0300

    atualizacao ListaSiplesEncadeada

--- Estrutura de Dados/ListaSiplesEncadeada/src/testaListaSequencial/Principal.java
+++ Estrutura de Dados/ListaSiplesEncadeada/src/testaListaSequencial/Principal.java
@@ -6,36 +6,39 @@ public class Principal {
 
 	public static void main(String[] args) {
 		ListaSequencial listaDePessoas = new ListaSequencial();
-		
-		Pessoa[] pessoa = new Pessoa[10];
-		
-		pessoa [0] = new Pessoa ("Ana");
-		pessoa [1] = new Pessoa ("Antonio");
-		pessoa [2] = new Pessoa ("Rodrigo");
-		pessoa [3] = new Pessoa ("Gabriel");
-		pessoa [4] = new Pessoa ("Mateus");
-		pessoa [5] = new Pessoa ("Vitor");
-		pessoa [6] = new Pessoa ("Pedro");
-		pessoa [7] = new Pessoa ("Petter");
-		pessoa [8] = new Pessoa ("Ricardo");
-		pessoa [9] = new Pessoa ("Ronaldo");
-		
+
+		Pessoa pessoa1 = new Pessoa("Ana");
+		Pessoa pessoa2 = new Pessoa("Antonio");
+		Pessoa pessoa3 = new Pessoa("Rodrigo");
+		Pessoa pessoa4 = new Pessoa("Gabriel");
+		Pessoa pessoa5 = new Pessoa("Mateus");
+		Pessoa pessoa6 = new Pessoa("Vitor");
+		Pessoa pessoa7 = new Pessoa("Pedro");
+		Pessoa pessoa8 = new Pessoa("Petter");
+		Pessoa pessoa9 = new Pessoa("Ricardo");
+		Pessoa pessoa10 = new Pessoa("Ronaldo");
+
 		System.out.println(listaDePessoas.tamanhoLista());
-		
-		for(int i = 0; i < 8; i++) {
-			listaDePessoas.adiciona(pessoa[i]);
+
+		listaDePessoas.adiciona(pessoa1);
+		listaDePessoas.adiciona(pessoa2);
+		listaDePessoas.adiciona(pessoa3);
+		listaDePessoas.adiciona(pessoa4);
+		listaDePessoas.adiciona(pessoa5);
+		listaDePessoas.adiciona(pessoa6);
+		listaDePessoas.adiciona(pessoa7);
+		listaDePessoas.adiciona(pessoa8);
+
+		for (int i = 0; i < listaDePessoas.tamanhoLista(); i++) {
+			System.out.println(listaDePessoas.getObject(i));
 		}
-		
-		for(int i = 0; i < listaDePessoas.tamanhoLista(); i++) {
-			System.out.println(pessoa[i]);
+
+		listaDePessoas.adiciona(3, pessoa9);
+
+		for (int i = 0; i < listaDePessoas.tamanhoLista(); i++) {
+			System.out.println(listaDePessoas.getObject(i));
 		}
-		
-		listaDePessoas.adiciona(3, pessoa[8]);
-		
-		for(int i = 0; i < listaDePessoas.tamanhoLista(); i++) {
-			System.out.println(pessoa[i]);
-		}
-		
-		System.out.println(listaDePessoas.contem(pessoa[1]));
+
+		System.out.println(listaDePessoas.contem(pessoa2));
 	}
 }
